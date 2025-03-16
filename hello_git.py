acce158204142373a1ffd66a089735e70c28bf8c
abdulghif <37424773+abdulghif@users.noreply.github.com>
2025-03-16 11:19:54 +0700

    Menambahkan fungsi pembagian

--- hello_git.py
+++ hello_git.py
@@ -2,3 +2,10 @@ print('Hello World and Hello Git!!')
 
 def fungsi_penambahan(a,b):
     return a+b
+
+
+def fungsi_pembagian(a,b):
+    if b > 0:
+        return a/b
+    else:
+        print("INVALID NUMBER RESULTS BECAUSE ZERO FOR DENOMINATOR")
